d96cf622ea5c6eb7672f99eda35f436446ffe352
mehrdadEsmaeli <mehrdadesmaili@gmail.com>
2024-09-15 19:34:07 +0330

    fix responsiv bog and create btn in faq compoonent

--- src/Components/Faq/Faq.jsx
+++ src/Components/Faq/Faq.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import "./Faq.css";
+
 
 function Faq() {
 
-
+    const ref =useRef();
+    const [toggle, settoggle] = useState(true);
     const [questionId, setQuestionId] = useState();
     const [questions, setQuestions] = useState([
         {
@@ -21,41 +23,59 @@ function Faq() {
     ]);
 
 
-    function clickHandler(questionID) {
-        if (questionID == questionId) {
-            setQuestionId("");
-        } else {
-            setQuestionId(questionID);
-        }
+function clickHandler(questionID) {
+    if (questionID == questionId) {
+        setQuestionId("");
+    } else {
+        setQuestionId(questionID);
     }
+}
 
-    return (
-        <div className="faq">
-            <div className="faq-container">
-                <div className="faq-div">
-                    <div className="faq__title">بیشترین سوال های پرسیده شده</div>
-                    <div className="faq__questions">
-                        {questions.map((question) => {
-                            return (
+const toggleHandler = (e) => {
+    settoggle((prev) => !prev);
+    e.target.parentNode.classList.toggle("circle-right")
+}
 
-                                <div key={question.id} className="faq__question-div">
-                                    <div className="faq__question">
-                                        <span className="faq__question-title">{question.question}</span>
-                                        <span onClick={() => { clickHandler(question.id) }} className="faq__question-icon">{questionId == question.id ? "-" : "+"}</span>
-                                    </div>
-                                    <div className={`faq__question-answer ${questionId == question.id && "faq__question-answer--activ"}`}>
-                                        <div className='faq__question-answer-box'>
-                                            {question.answer}
+return (
+    <div className="faq">
+        <div className="faq-container">
+            <div className="faq-div">
+                <div className="faq-toggle ">
+                    <span className="faq-toggle__text">سوال ها</span>
+                    <div onClick={toggleHandler} className="faq-toggle__btn"></div>
+                    <span className="faq-toggle__text">پلن ها</span>
+                </div>
+                {toggle ? (
+                    <>
+                        <div className="faq__title">بیشترین سوال های پرسیده شده</div>
+                        <div className="faq__questions">
+                            {questions.map((question) => {
+                                return (
+
+                                    <div key={question.id} className="faq__question-div">
+                                        <div className="faq__question">
+                                            <span className="faq__question-title">{question.question}</span>
+                                            <span onClick={() => { clickHandler(question.id) }} className="faq__question-icon">{questionId == question.id ? "-" : "+"}</span>
+                                        </div>
+                                        <div className={`faq__question-answer ${questionId == question.id && "faq__question-answer--activ"}`}>
+                                            <div className='faq__question-answer-box'>
+                                                {question.answer}
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            )
-                        })}
-                    </div>
-                </div>
+                                )
+                            })}
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <div>plans</div>
+                    </>
+                )}
             </div>
         </div>
-    )
+    </div>
+)
 }
 
 export default Faq 
--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -1,14 +1,7 @@
 import React, { useEffect } from 'react';
-import Parallax from 'parallax-js';
 import "./News.css";
 
 function News() {
-    useEffect(()=>{
-        let foterScene = document.getElementById('foter-scene');
-        let parallaxFoter = new Parallax(foterScene);
-
-    },[])
-
 
   return (
     <div className="news">
@@ -20,10 +13,6 @@ function News() {
                     <a href="" className="news__btn btn">عضویت</a>
                 </div>
             </div>
-        </div>
-        <div className="news__icons" id='foter-scene'>
-            <div data-depth="0.02" className="news__icon1"><img src="./images/helix2 1.png" alt="" /></div>
-            <div data-depth="0.04" className="news__icon2"><img src="./images/emojistar 1.png" alt="" /></div>
         </div>
     </div>
   )
